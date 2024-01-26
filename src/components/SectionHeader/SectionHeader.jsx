68a88e2b434d8514405da92277483c2075f1b6ad
HenriqueDelben <hd.delben@gmail.com>
2024-01-26 17:22:40 -0300

    :sparkles: feat: Add "SectionHeader" component

--- src/components/SectionHeader/SectionHeader.jsx
+++ src/components/SectionHeader/SectionHeader.jsx
@@ -1,13 +1,16 @@
 import styles from './SectionHeader.module.css';
 import classnames from 'classnames/bind';
+import PropTypes from "prop-types";
 
 const cx = classnames.bind(styles);
 
-export default function SectionHeader({title, content, textColor} ) {
+export default function SectionHeader({title, content, textColor, position, heading} ) {
 
     return (
         <div className={cx("SectionHeader", {
-            [`SectionHeader___${textColor}`]: textColor
+            [`SectionHeader___${textColor}`]: textColor,
+            [`SectionHeader___${position}`]: position,
+            [`SectionHeader___${heading}`]: heading
         })}>
             <div className={styles.SectionHeader_h1Container}>
                 <h1 className={styles.SectionHeader_h1}>{title}</h1>
@@ -17,7 +20,10 @@ export default function SectionHeader({title, content, textColor} ) {
     );
 }
 
-/*
-    MODIFICACOES
-    - tamanho do componente como um todo: normal, big, small
-*/
+SectionHeader.propTypes = {
+    title: PropTypes.string,
+    content: PropTypes.string,
+    textColor: PropTypes.oneOf(["black", "white"]),
+    position: PropTypes.oneOf(["left", "center"]),
+    heading: PropTypes.oneOf(["normal", "small"])
+}
